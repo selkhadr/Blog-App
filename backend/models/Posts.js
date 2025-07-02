d9154ba5a81782c25b2cdf611ee9884a3fe329ca
selkhadr <elkhadrisara01@gmail.com>
2025-07-02 16:04:09 +0100

    add Post mode

---
+++ backend/models/Posts.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const joi = require("joi");
+const PostSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength: 2,
+        maxlength: 200,
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength: 20,
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:Object,
+        default:{
+            url: "",
+            publicId:null,
+        }
+    },
+    likes:[
+        {
+            type:mongoose.Schema.types.ObjectId,
+            ref:"User"
+        }
+    ]
+},{timestamps:true,});
+
+const Post = mongoose.model("Post", PostSchema);
+
+function validateCreatePost(obj){
+    const schema = joi.object({
+        title: joi.string().trim().min(2).max(200).required(),
+        description: joi.string().trim().min(10).required(),
+        category: joi.string().trim().required(),
+    }) ;
+    return schema.validate(obj);
+}
+
+function validateUpdatePost(obj){
+    const schema = joi.object({
+        title: joi.string().trim().min(2).max(200),
+        description: joi.string().trim().min(10),
+        category: joi.string().trim(),
+    }) ;
+    return schema.validate(obj);
+}
+
+module.exports={Post,validateCreatePost,validateUpdatePost}
