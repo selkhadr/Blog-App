2ef71a67fed5c376d0667a3fcb960a9dbe32faf7
selkhadr <elkhadrisara01@gmail.com>
2025-07-02 19:59:37 +0100

    update post & update post image

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const asyncHandler = require("express-async-handler");
-const {Post,validateCreatePost} = require("../models/Posts");
-const {cloudinaryUploadImage}=require("../utils/cloudinary");
+const {Post,validateCreatePost, validateUpdatePost} = require("../models/Posts");
+const {cloudinaryUploadImage, cloudinaryRemoveImage}=require("../utils/cloudinary");
 const { json } = require("stream/consumers");
-
+ 
 /**
  *@desc create new post
  * @router /api/posts
@@ -86,3 +86,87 @@ module.exports.getPostCountCtrl = asyncHandler(async(req,res)=>{
     res.status(200).json(count);
 })
 
+/**
+ *@desc delete post
+ * @router /api/posts/:id
+ * @method delete
+ * @access private (only admin or owner of the post)
+ */
+module.exports.deletePostCtrl = asyncHandler(async(req,res)=>{
+    const post = await Post.findById(req.params.id);
+    if(!post){
+        return res.status(404).json({message: "post not found"});
+    }
+    if(req.user.isAdmin || req.user.id === post.user.toString()){
+        await Post.findByIdAndDelete(req.params.id);
+        await cloudinaryRemoveImage(post.image.publicId);
+
+        res.status(200).json({message: "post has been deleted successfully", postId:post._id});
+    }else{
+        res.status(403).json({message: "access denied, forbidden"});
+
+    }
+})
+
+/**
+ *@desc update post
+ * @router /api/posts/:id
+ * @method put
+ * @access private (only owner of the post)
+ */
+module.exports.updatePostCtrl = asyncHandler(async(req,res)=>{
+    const {error} = validateUpdatePost(req.body);
+    if(error){
+        return res.status(400).json({message:error.details[0].message});
+    }
+    const post = await Post.findById(req.params.id);
+    if(!post){
+        return res.status(404).json({message:"post not found" })
+    }
+    if(req.user.id !== post.user.toString()){
+        return res.status(403).json({message:"access denied; you are not allowed"})
+    }
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id,{
+        $set:{
+            title:req.body.title,
+            description:req.body.description,
+            category:req.body.category
+        }
+    }, {new:true}).populate("user", ["-password"])
+    res.status(200).json(updatedPost);
+});
+
+/**
+ *@desc update post image
+ * @router /api/posts/upload-image/:id
+ * @method put
+ * @access private (only owner of the post)
+ */
+module.exports.updatePostImageCtrl = asyncHandler(async(req,res)=>{
+
+    if(!req.file){
+        return res.status(400).json({message:"no image was provided"});
+    }
+    const post = await Post.findById(req.params.id);
+    if(!post){
+        return res.status(404).json({message:"post not found" })
+    }
+    if(req.user.id !== post.user.toString()){
+        return res.status(403).json({message:"access denied; you are not allowed"})
+    }
+    await cloudinaryRemoveImage(post.image.publicId);
+    const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
+    const result = await cloudinaryUploadImage(imagePath);
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id,{
+    $set:{
+        image:{
+            url:result.secure_url,
+            publicId:result.public_id,
+        }
+    }
+    }, {new:true}).populate("user", ["-password"])
+
+    
+    res.status(200).json(updatedPost);
+    fs.unlinkSync(imagePath);
+});
--- backend/routes/postsRoute.js
+++ backend/routes/postsRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const photoUpload = require("../middlewares/photoUpload");
 const {verifyToken}=require("../middlewares/verifyToken");
-const {createPostCtr, getAllPostsCtrl, getSinglePostCtrl, getPostCountCtrl}=require("../controllers/postController");
+const {createPostCtr, getAllPostsCtrl, getSinglePostCtrl, getPostCountCtrl, deletePostCtrl, updatePostCtrl, updatePostImageCtrl}=require("../controllers/postController");
 const validateObjectId = require("../middlewares/validateObjectId");
 router.route("/")
     .post(verifyToken, photoUpload.single("image"),createPostCtr)
@@ -9,5 +9,10 @@ router.route("/")
     
 router.route("/count").get(getPostCountCtrl);
 
-router.route("/:id").get(validateObjectId, getSinglePostCtrl);
+router.route("/:id").get(validateObjectId, getSinglePostCtrl)
+.delete(validateObjectId, verifyToken, deletePostCtrl)
+.put(validateObjectId, verifyToken,updatePostCtrl);
+
+router.route("/update-image/:id")
+.put(validateObjectId, verifyToken, photoUpload.single("image"),updatePostImageCtrl)
 module.exports =router;
